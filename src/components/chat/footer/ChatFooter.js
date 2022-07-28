58fbc58af2003809f0c037e6162087c67b098c27
Shivangam Soni <shivangamsoni99@gmail.com>
2022-07-28 19:25:51 +0530

    Add: Chat Panel Footer

--- src/components/chat/footer/ChatFooter.js
+++ src/components/chat/footer/ChatFooter.js
@@ -1,9 +1,40 @@
+import { IconButton } from "@mui/material";
+
+import InsertEmoticonOutlinedIcon from "@mui/icons-material/InsertEmoticonOutlined";
+import AttachFileOutlinedIcon from "@mui/icons-material/AttachFileOutlined";
+import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
+
 import styles from "./styles.module.css";
 
 const ChatFooter = () => {
   return (
     <footer className={styles.footer}>
-      <h1>ChatFooter</h1>
+      <div className={styles.footer__actions}>
+        <div className={styles.action}>
+          <IconButton>
+            <InsertEmoticonOutlinedIcon />
+          </IconButton>
+        </div>
+
+        <div className={styles.action}>
+          <IconButton>
+            <AttachFileOutlinedIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className={styles.footer__input}>
+        <div className={styles.input__group}>
+          <input type="text" />
+          <span className={styles.input__placeholder}>Type a message</span>
+        </div>
+
+        <div className={styles.input__btn}>
+          <IconButton>
+            <SendOutlinedIcon />
+          </IconButton>
+        </div>
+      </div>
     </footer>
   );
 };
