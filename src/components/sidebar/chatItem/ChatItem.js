4e3748a59964270aea2b1f4a75cc70ff258f1e3d
Shivangam Soni <shivangamsoni99@gmail.com>
2022-07-28 12:19:38 +0530

    Add: Sidebar Chat Item

--- src/components/sidebar/chatItem/ChatItem.js
+++ src/components/sidebar/chatItem/ChatItem.js
@@ -1,12 +1,25 @@
 import Avatar from "@mui/material/Avatar";
+import { useEffect, useState } from "react";
 
 import styles from "./styles.module.css";
 
 const SidebarChatItem = () => {
+  const [id, setId] = useState();
+  useEffect(() => {
+    setId(Math.floor(Math.random() * 99999));
+  }, []);
+
   return (
-    <li>
-      <div>
-        <Avatar />
+    <li className={styles.chat}>
+      <div className={styles.chat__avatar}>
+        <Avatar
+          src={`https://avatars.dicebear.com/api/bottts/${id}.svg?b=%23e9edef`}
+        />
+      </div>
+
+      <div className={styles.chat__info}>
+        <h2 className={styles.info__user}>Chat Name</h2>
+        <p className={styles.info__message}>Last Message...</p>
       </div>
     </li>
   );
