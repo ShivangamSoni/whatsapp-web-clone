360c500ea2a10c28b4bdde85f8a0757cb92cca96
Shivangam Soni <shivangamsoni99@gmail.com>
2022-07-28 21:06:41 +0530

    Add: Scroll Button

--- src/components/chat/body/ChatBody.js
+++ src/components/chat/body/ChatBody.js
@@ -1,3 +1,7 @@
+import IconButton from "@mui/material/IconButton";
+
+import ExpandMoreOutlinedIcon from "@mui/icons-material/ExpandMoreOutlined";
+
 import styles from "./styles.module.css";
 
 import ChatMessage from "../chatMessage/ChatMessage";
@@ -7,9 +11,15 @@ const ChatBody = () => {
     <main className={styles.body}>
       <div></div>
       <div className={styles.messages}>
-        {[...Array(100).keys()].map((idx) => (
+        {[...Array(10).keys()].map((idx) => (
           <ChatMessage key={idx} isSend={idx % 2} />
         ))}
+
+        <div className={styles.scroll}>
+          <IconButton>
+            <ExpandMoreOutlinedIcon />
+          </IconButton>
+        </div>
       </div>
     </main>
   );
