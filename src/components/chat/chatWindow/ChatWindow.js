7f09b77059dd2788096a2160bf6e417854f957f0
Shivangam Soni <shivangamsoni99@gmail.com>
2022-07-28 13:10:35 +0530

    Add: Chat Window Footer Comp

--- src/components/chat/chatWindow/ChatWindow.js
+++ src/components/chat/chatWindow/ChatWindow.js
@@ -1,11 +1,14 @@
 import styles from "./styles.module.css";
 
 import ChatHeader from "../header/ChatHeader";
+import ChatFooter from "../footer/ChatFooter";
 
 const ChatWindow = () => {
   return (
     <div className={styles.window}>
       <ChatHeader />
+      <main></main>
+      <ChatFooter />
     </div>
   );
 };
---
+++ src/components/chat/footer/ChatFooter.js
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+const ChatFooter = () => {
+  return (
+    <footer className={styles.footer}>
+      <h1>ChatFooter</h1>
+    </footer>
+  );
+};
+
+export default ChatFooter;
