551f9451c53d5c2c7fe7dbb29a5660c7a2150ec4
Shivangam Soni <shivangamsoni99@gmail.com>
2022-08-04 17:20:46 +0530

    Add: Login UI

--- src/features/login/login.js
+++ src/features/login/login.js
@@ -1,15 +1,25 @@
 import { signInWithRedirect } from "firebase/auth";
 import { auth, provider } from "../../firebase/firebase";
 
+import styles from "./styles.module.css";
+
+import { ReactComponent as GoogleIcon } from "../../assets/google_icon.svg";
+import { ReactComponent as WhatsappLogo } from "../../assets/whatsapp_logo.svg";
+
 const Login = () => {
   const signIn = () => {
     signInWithRedirect(auth, provider);
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <button onClick={signIn}>Sign In using Google</button>
+    <div className={styles.login}>
+      <div className={styles.login__logo}>
+        <WhatsappLogo width="inherit" height="inherit" />
+      </div>
+      <h1 className={styles.login__title}>Start Using WhatsApp Clone</h1>
+      <button className={styles.login__btn} onClick={signIn}>
+        <GoogleIcon /> <span>Sign in with Google</span>
+      </button>
     </div>
   );
 };
