f25d834446508cc58f6957627fc98ea6677d0be4
Shivangam Soni <shivangamsoni99@gmail.com>
2022-08-04 19:55:22 +0530

    Add: Last Message

--- src/components/sidebar/chatItem/ChatItem.js
+++ src/components/sidebar/chatItem/ChatItem.js
@@ -3,10 +3,36 @@ import { useNavigate, useMatch } from "react-router-dom";
 import Avatar from "@mui/material/Avatar";
 
 import styles from "./styles.module.css";
+import { useEffect, useState } from "react";
+import {
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import db from "../../../firebase/firebase";
 
 const SidebarChatItem = ({ id, data: { name } }) => {
   const navigate = useNavigate();
   const active = useMatch(`/rooms/${id}`);
+  const [lastMessage, setLastMessage] = useState("");
+
+  useEffect(() => {
+    const messagesQuery = query(
+      collection(db, `chatrooms/${id}/messages`),
+      orderBy("timestamp", "desc"),
+      limit(1),
+    );
+
+    const msgUnsub = onSnapshot(messagesQuery, (snapshot) => {
+      snapshot.forEach((doc) => setLastMessage(doc.data().message));
+    });
+
+    return () => {
+      msgUnsub();
+    };
+  }, [id]);
 
   return (
     <li
@@ -21,7 +47,11 @@ const SidebarChatItem = ({ id, data: { name } }) => {
 
       <div className={styles.chat__info}>
         <h2 className={styles.info__user}>{name}</h2>
-        <p className={styles.info__message}>Last Message...</p>
+        <p className={styles.info__message}>
+          {lastMessage.length > 10
+            ? lastMessage.substring(0, 10) + "..."
+            : lastMessage}
+        </p>
       </div>
     </li>
   );
